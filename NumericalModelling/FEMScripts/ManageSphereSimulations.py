0704e1fd7c81757880ab5c86ccbf25abbdbaffbf
Bert Van Raemdonck <BertVanRaemdonck@users.noreply.github.com>
2023-11-02 21:09:04 +0100

    Add comments to ManageSphereSimulations.py

--- NumericalModelling/FEMScripts/ManageSphereSimulations.py
+++ NumericalModelling/FEMScripts/ManageSphereSimulations.py
@@ -20,14 +20,32 @@ import datetime
 from abaqusConstants import STANDARD_EXPLICIT
 
 # Set up directory system
-baseFolder = r'C:\abaquswork\SphereSimulationMaster'
+# FILL THIS IN:
+#	baseFolder is the path on your local file system that serves
+#	as the main folder of this script. You can chose any path to
+# 	which you have write access. You have to make sure that the
+# 	selected baseFolder contains the following three subfolders
+# - baseFolder/Scripts
+#	this folder contains all the script files that you downloaded,
+#	including this very file
+# - baseFolder/SimulationFiles
+#	this folder is initially empty, but will come to contain all
+#	the temporary Abaqus file as it is running which allows you
+#	to follow the status of the simulation and debug it.
+# - baseFolder/Results
+#	this is the folder to which a json file with the simulation
+#	results will be written
+baseFolder = r'C:\Users\XXXX\Downloads\Buckling-Capsule-Metafluids-main\NumericalModelling'
 scriptFolder = os.path.join(baseFolder, r'Scripts')
 simFolder = os.path.join(baseFolder, r'SimulationFiles')
-outFolder = os.path.join(baseFolder, r'Results\EllipsoidImperfection')
+outFolder = os.path.join(baseFolder, r'Results')
 
 # import custom functions
+# FILL THIS IN:
+#	the absolute path to the Dependencies folder should be
+#	included here so the python modules can be imported
 sys.path.append(scriptFolder)
-sys.path.append(r'I:\PhD\Utilities\Python\AbaqusParsers')
+sys.path.append(r'C:\Users\XXXX\Downloads\Buckling-Capsule-Metafluids-main\NumericalModelling\FEMScripts\Dependencies')
 
 import AbaqusScriptUtilities as scrpt
 from CreateSphereSimulation import generateSphereModel
@@ -47,58 +65,90 @@ odbLocation = os.path.join(simFolder, '{}.odb'.format(modName))
 #   sigma = imperfection dimple standard deviation in angle
 #   load_max = maximum change in volume/initial volume
 
+# Definition of constants
 SOLID = 'solid'
 SHELL = 'shell'
 AXI = 'axisymmetric'
 THREED = '3D'
 PIN = 'pin'
 AVERAGE = 'average'
+WRITEINP = 0
+EXECUTE = 1
 
-etas = np.arange(.1, 1.5, .1)
+# FILL THIS IN
+#	simControls is the main variable that controls which simulations
+#	are run with which variables. It relies on a custom class
+#	controlParameters that is defined in the AbaqusScripUtilities.py
+#	file in the dependencies folder. It allows to define the
+#	parameters for the simulations as a dictionary with as keys the
+#	names of the parameters and as values either single values or
+#	lists of values. In case a list is provided, one simulation will
+#	be run for every value of that parameter in the list. In case a
+# 	single value is provided, that parameter will stay constant
+#	throughout all simulations. It is possible to enter lists for
+# 	multiple parameters, but they should all have the same length.
 
-# calculate parameters for a shell corresponding to the CT measurement
-# all dimensions in µm
-posInner = np.array([289.6, 283.6, 269.7])
-posOuter = np.array([306.0, 287.7, 269.6])
-rInner = 174.85
-rOuter = 240.7
-
-rMean = .5*(rInner+rOuter)
-t = rOuter-rInner
-offset = np.linalg.norm(posInner-posOuter)
-
-eta = t/rMean
-ecc = offset/t
-
-#tMin = t - offset
-#eta = tMin/rMean
-
-#etas = np.array([.002, .004, .006, .008])
-#np.array([1./9.5, 2./9., 2.3/8.85, 2.5/8.75, 3.0/8.5, 4.0/8.0, 4.5/7.75, 5.0/7.5])
-
-etas = np.logspace(-2, 0, 21)[18:19]
-etas = np.array([.01*(10**.1)**(2*i) for i in [0,]]) #[0,3,6,9]])[3:]#range(12)])
-etas = np.array([0.5011872336272725,])
-
+# 	In all simulations, the midline radius of the capsule is 1 and
+#	the shear modulus is 1. The other parameters that have to be
+#	defined are the following:
+#	- 'eta': shell thickness
+#	- 'delta': magnitude of the imperfection. The imperfection is
+#		   a dimple shaped as a Gaussian bell curve centered at 
+#                  the pole and displacing all points radially inward
+#	- 'sigma': standard deviation (in radians) of the bell curve
+#                  defining the imperfection
+#       - 'ecc': eccentricity of the capsule, namely the distance
+#                between the center point of the outer surface of the
+#                capsule and the center point of the inner surface of
+#                the inner surface. 0 for a concentric shell. Must be
+#                below eta
+#	- 'alpha': ellipticity parameter, namely the radius between the
+#                  minor and the major radius of the capsule in case
+#                  the shape is an oblate ellipsoid instead of a sphere.
+#                  1 for a perfect sphere
+# 	- 'load_max': (approximate) ratio between the maximal change in
+#		      volume of the capsule and the initial internal
+#   		      volume
+# 	- 'element_type': SOLID for solid elements, SHELL for shell
+# 			  elements
+# 	- 'dimensionality': AXI for an axisymmetric simulation, 3D for
+#                           a fully three dimensional simulation
+#    	- 'clamping': type of boundary condition to constrain the
+#                     rigid-body vertical motion of the capsule. PIN
+#                     for pinning the anitpole, AVERAGE for constraining
+#                     the average vertical displacement of all nodes
+#                     using a distributing coupling constraint
+# 	- 'it_max': maximal number of iterations in the simulation
+# 	- 'step_max': maximal time increment size in the simulation,
+# 		      taking into account that the total step time is 10
+# 	- 'mesh_size': maximal seed size for the mesh generation
+etas = np.array([2./9.,])
 simControls = scrpt.controlParameters({
-				'eta': etas, #0.2265,#[.22, .24, .26, .28, .3],
-				'delta': .005*etas, #[3*min(.04, .05*eta) for eta in etas],#.02*eta, #2 * 0.005, #5e-2*etas,
-				'sigma': np.pi/6., #[min(radians(60), 1.5/np.sqrt(np.sqrt(1-.5**2)/eta)) for eta in etas], #radians(80),
-				'ecc': 0., #0.106, #0.,
+				'eta': etas,
+				'delta': .005*etas,
+				'sigma': np.pi/6.,
+				'ecc': 0., 
 				'alpha': 1.,
-				'load_max': .999, #[.97 if h < .5 else .72*.75 for h in etas], #.97, #.75,
+				'load_max': [.97 if h < .5 else .75 for h in etas],
 				'element_type': [SOLID if eta > .05 else SHELL for eta in etas],
 				'dimensionality': AXI,
 				'clamping': PIN,
 				'it_max': 2500,
-				'step_max': .03,   # total step time is 10
-				'mesh_size': [max(eta/20, .03) for eta in etas] #.5*.06#.055 #[max(.01, min(.06, .125*eta)) for eta in etas]
+				'step_max': .03,
+				'mesh_size': [max(eta/20, .03) for eta in etas]
 				})
 
-# constants
-WRITEINP = 0
-EXECUTE = 1
-# simulation controls
+# FILL THIS IN
+#	define what to do with the simulations:
+#       - EXECUTE executes all simulations one after the other.
+#	          the Abaqus interface will not respond until all
+#                 simulations have been completed, but the progress
+#                 can be tracked through the messages in the Abaqus
+#                 terminal or in the .dat file in the SimulationFiles
+# 		  folder
+# 	- WRITEINP just writes the input file for every simulation
+#  	    	   to the SimulationFilesFolder without running the
+#		   the simulations.
 task = EXECUTE
 
 # -----------------------
